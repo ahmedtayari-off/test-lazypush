452c0d6f731804c06caff247eb7ed568050a0094
Ahmed Tayari <ahmed.tayari.off@gmail.com>
2026-05-16 20:54:50 +0100

    chore: update Terraform architecture

--- main.tf
+++ main.tf
@@ -19,11 +19,11 @@ module "app_alb" {
 
   name = var.app_alb_name
   internal = var.app_alb_internal
-  listeners = var.app_alb_listeners
-  tags = var.app_alb_tags
-  target_groups = var.app_alb_target_groups
-  security_groups = module.sg_alb.security_group_id
   load_balancer_type = var.app_alb_load_balancer_type
+  security_groups = module.sg_alb.security_group_id
+  target_groups = var.app_alb_target_groups
+  tags = var.app_alb_tags
+  listeners = var.app_alb_listeners
   vpc_id = module.main_vpc.vpc_id
   subnets = module.main_vpc.public_subnets
 }
@@ -32,6 +32,8 @@ module "app_service" {
   source = "./modules/terraform-aws-ecs"
 
   cluster_name = var.app_service_cluster_name
+  autoscaling_capacity_providers = var.app_service_autoscaling_capacity_providers
+  fargate_capacity_providers = var.app_service_fargate_capacity_providers
   services = {
       app-service = {
       create_security_group = false
@@ -43,10 +45,8 @@ module "app_service" {
       subnet_ids = module.main_vpc.private_subnets
     }
     }
-  tags = var.app_service_tags
-  fargate_capacity_providers = var.app_service_fargate_capacity_providers
   default_capacity_provider_use_fargate = var.app_service_default_capacity_provider_use_fargate
-  autoscaling_capacity_providers = var.app_service_autoscaling_capacity_providers
+  tags = var.app_service_tags
 }
 
 module "app_db" {
@@ -62,11 +62,11 @@ module "app_db" {
   allocated_storage = var.app_db_allocated_storage
   deletion_protection = var.app_db_deletion_protection
   skip_final_snapshot = var.app_db_skip_final_snapshot
-  subnet_ids = module.main_vpc.database_subnets
-  tags = var.app_db_tags
-  storage_encrypted = var.app_db_storage_encrypted
   vpc_security_group_ids = module.sg_rds.security_group_id
   engine = var.app_db_engine
+  subnet_ids = module.main_vpc.database_subnets
+  storage_encrypted = var.app_db_storage_encrypted
+  tags = var.app_db_tags
   db_subnet_group_name = module.main_vpc.database_subnet_group_name
 }
 
@@ -77,11 +77,11 @@ module "app_cache" {
   engine_version = var.app_cache_engine_version
   num_cache_clusters = var.app_cache_num_cache_clusters
   replication_group_id = var.app_cache_replication_group_id
+  at_rest_encryption_enabled = var.app_cache_at_rest_encryption_enabled
   tags = var.app_cache_tags
+  engine = var.app_cache_engine
   security_group_ids = module.sg_redis.security_group_id
   transit_encryption_enabled = var.app_cache_transit_encryption_enabled
-  engine = var.app_cache_engine
-  at_rest_encryption_enabled = var.app_cache_at_rest_encryption_enabled
   subnet_group_name = module.main_vpc.elasticache_subnet_group_name
 }
 
@@ -89,10 +89,10 @@ module "app_assets" {
   source = "./modules/terraform-aws-s3-bucket"
 
   bucket = var.app_assets_bucket
-  lifecycle_rule = var.app_assets_lifecycle_rule
-  server_side_encryption_configuration = var.app_assets_server_side_encryption_configuration
-  tags = var.app_assets_tags
   versioning = var.app_assets_versioning
+  lifecycle_rule = var.app_assets_lifecycle_rule
+  tags = var.app_assets_tags
+  server_side_encryption_configuration = var.app_assets_server_side_encryption_configuration
 }
 
 module "sg_alb" {
@@ -125,8 +125,8 @@ module "ecs_task_role" {
   create_instance_profile = var.ecs_task_role_create_instance_profile
   custom_role_policy_arns = var.ecs_task_role_custom_role_policy_arns
   create_role = var.ecs_task_role_create_role
-  tags = var.ecs_task_role_tags
   role_requires_mfa = var.ecs_task_role_role_requires_mfa
+  tags = var.ecs_task_role_tags
 }
 
 module "sg_rds" {
